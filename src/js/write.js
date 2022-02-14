c0cb4c7af0f3705dcc75cf9e5190f7af1c6b7c71
CelinaTran <celinatran98@gmail.com>
2022-02-13 22:59:49 -0700

    added ability to upload and displat the uploaded photo

--- src/js/write.js
+++ src/js/write.js
@@ -1,6 +1,22 @@
-import{ref, push} from 'firebase/database'
-import{db} from "../libs/firebase/firebaseConfig"
+import { ref as storageRef, uploadBytes, getDownloadURL } from "firebase/storage";
+import {ref as databaseRef, push, set, get} from 'firebase/database'
+import { db, storage  } from "../libs/firebase/firebaseConfig";
 
+function uploadImage(event) 
+{
+    let image = event.target.files[0];
+
+    document.querySelector(".product-image img").src = URL.createObjectURL(image);
+
+    checkImageUpload(image);
+}
+
+async function checkImageUpload(image)
+{
+    const imageRef = storageRef(storage, image.name);
+    const confirmation = await uploadBytes(imageRef, image);
+    console.log(confirmation);
+}
 
 
 function onAddNewItem(event)
@@ -10,28 +26,32 @@ function onAddNewItem(event)
     const productName = event.target.elements['productName'].value.trim();
     const productPrice = event.target.elements['productPrice'].value;
     const productCategory = event.target.elements['productCategory'].value.trim();
-    const onSale = event.target.elements['onSale'];
+    const Sale = event.target.elements['onSale'];
 
-    if(onSale.value.checked)
+    if(Sale.checked)
     {
-        console.log(checked);
+        Sale.value = true;
+    }
+    else
+    {
+        Sale.value = false;
     }
 
     const dbCategory = `products/ ${productCategory}`;
 
-    const docRef = ref(db, dbCategory)
+    const docRef = databaseRef(db, dbCategory)
 
     const newRef = push(docRef, 
         {
             productName: productName,
             price:productPrice,
             category: productCategory,
-            sale:onSale
+            sale:Sale
             
         })
 
 }
 
-
+document.querySelector("#productImage").addEventListener("change", uploadImage);
 document.forms['addNewProductForm']
         .addEventListener("submit", onAddNewItem);
